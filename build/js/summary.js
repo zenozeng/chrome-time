88479f0414d6f8cd195b6e66fcc9f6dccaa3fd1f
Zeno Zeng <zenoes@qq.com>
2013-03-14 18:25:14 +0800

    update summary view

--- build/js/summary.js
+++ build/js/summary.js
@@ -5,33 +5,29 @@ $(document).ready(function() {
     var html = '';
     var opt = localStorage.getItem('opt') != null ? JSON.parse(localStorage.getItem('opt')) : {};
 
-    if(typeof(opt['limit']) != "undefined") {
-        for(var i=0; i<opt['limit'].length; i++) {
-            var item = opt['limit'][i]['domain'];
-            html += '<div style="float:left;box-shadow: 0 0 8px #777; margin: 1em; padding: 1em; line-height: 1.5em;">';
-            html += '<a href="detail.html#'+encodeURIComponent(JSON.stringify(item))+'" style="text-decoration:none">';
-            html += '<span style="color: blue">[*] '+JSON.stringify(item)+'</span>';
-            html += '</a>';
-            html += '\n总计：' + formatTimeDelta(clock.getSum(item));
-            html += '\n本月：' + formatTimeDelta(clock.getSumThisMonth(item));
-            html += '\n本周：' + formatTimeDelta(clock.getSumThisWeek(item));
-            html += '\n今日：' + formatTimeDelta(clock.getSumToday(item));
-            html += '</div>';
-        }
-    }
-
-    html += '<div style="clear:left"></div>';
-
     for(var i=0; i<items.length; i++) {
         var item = items[i];
-        html += '<div style="float:left;box-shadow: 0 0 8px #777; margin: 1em; padding: 1em; line-height: 1.5em;">';
-        html += '<a href="detail.html#'+encodeURIComponent(JSON.stringify(item))+'" style="text-decoration:none">';
+        html += '<div style="border:1px solid #777; max-width:850px;margin:0 auto;border-bottom:#fff;  padding: 1em; line-height: 1.5em; font-size:12px;">';
+        html += '<a href="detail.html#'+encodeURIComponent(JSON.stringify(item))+'" style="text-decoration:none;">';
         html += '<span style="color: blue">#'+i+" "+JSON.stringify(item)+'</span>';
         html += '</a>';
-        html += '\n总计：' + formatTimeDelta(clock.getSum(item));
-        html += '\n本月：' + formatTimeDelta(clock.getSumThisMonth(item));
-        html += '\n本周：' + formatTimeDelta(clock.getSumThisWeek(item));
-        html += '\n今日：' + formatTimeDelta(clock.getSumToday(item));
+        html += '<span style="float:right">';
+        html += '<span style="width:90px; display:inline-block;text-align:left;">';
+        html += '总计：' + formatTimeDelta(clock.getSum(item));
+        html += '</span>';
+        html += '&nbsp;&nbsp;';
+        html += '<span style="width:90px; display:inline-block;text-align:left;">';
+        html += '本月：' + formatTimeDelta(clock.getSumThisMonth(item));
+        html += '</span>';
+        html += '&nbsp;&nbsp;';
+        html += '<span style="width:80px; display:inline-block;text-align:left;">';
+        html += '本周：' + formatTimeDelta(clock.getSumThisWeek(item));
+        html += '</span>';
+        html += '&nbsp;&nbsp;';
+        html += '<span style="width:80px; display:inline-block;text-align:left;">';
+        html += '今日：' + formatTimeDelta(clock.getSumToday(item));
+        html += '</span>';
+        html += '</span>';
         html += '</div>';
     }
     $('#items').html(html);
